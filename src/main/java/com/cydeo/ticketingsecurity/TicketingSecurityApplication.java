c8badaa09e6c112724936e14754099cc344cda6f
symcetin <seyma.oskurcu@gmail.com>
2023-01-13 22:25:21 -0500

    Initial commit

---
+++ src/main/java/com/cydeo/ticketingsecurity/TicketingSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo.ticketingsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketingSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cydeo/ticketingsecurity/TicketingSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.ticketingsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
